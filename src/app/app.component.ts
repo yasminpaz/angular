49f78f1f07254938017b284a4da6ee1d416d75e1
Yasmin <yasmin.spaz@hotmail.com>
2021-07-16 11:19:02 -0300

    Conceito de One way data binding e two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-introducao-angular';
+  title = 'Meu titulo';
+
+  name: string = 'Yasmin';
 }
